2f73c5d3ab3883dea8fe5029234988fdff2ce6c2
Lukeuke <lukasz.mrowiec10102004@gmail.com>
2025-05-19 21:20:25 +0200

    #1 Added ability to playback music

--- src/AudioVisualizer.cpp
+++ src/AudioVisualizer.cpp
@@ -7,12 +7,22 @@
 #include <iostream>
 #include <ostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 void AudioVisualizer::run() const {
     std::cout << "Running AudioVisualizer..." << std::endl;
 
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Audio Visualizer");
+    auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "Audio Visualizer");
     window.setFramerateLimit(144);
+
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("./_support/input.wav")) {
+        std::cerr << "Failed to load audio file" << std::endl;
+        return;
+    }
+
+    sf::Sound sound(buffer);
+    sound.play();
 
     while (window.isOpen())
     {
